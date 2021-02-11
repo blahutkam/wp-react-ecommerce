e1211f26201da47d97c0814c4b7a0ca30c0bc9fa
Martin Blahutka <blahec@Martins-MacBook-Pro.local>
2021-02-11 13:59:45 -0800

    homepage basic template and style

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./homepage.styles.scss";
+import React, { Fragment } from "react";
+
+import HomePage from "./homepage.component";
+
+function App() {
+  return (
+    <Fragment>
+      <HomePage />
+    </Fragment>
+  );
+}
+
+export default App;

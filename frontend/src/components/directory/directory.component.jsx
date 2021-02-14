367c489b29281d015d60a4b64724002ed55174f0
Martin Blahutka <blahec@Martins-MacBook-Pro.local>
2021-02-14 13:21:30 -0800

    withRouter HOC

--- frontend/src/components/directory/directory.component.jsx
+++ frontend/src/components/directory/directory.component.jsx
@@ -46,9 +46,22 @@ export class Directory extends Component {
 
   render() {
     return (
+      // <div className="directory-menu">
+      //   {this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+      //     <MenuItem
+      //       key={id}
+      //       title={title}
+      //       imageUrl={imageUrl}
+      //       size={size}
+      //       linkUrl={linkUrl}
+      //     />
+      //   ))}
+      // </div>
+
+      //using spread operator
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
--- frontend/src/components/menu-item/menu-item.component.jsx
+++ frontend/src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,23 @@
-//import "../../scss/main.scss";
 import React from "react";
 
-const MenuItem = ({ title, imageUrl, size }) => {
-  return (
-    <div className={`${size} menu-item`}>
-      <div
-        style={{ backgroundImage: `url(${imageUrl})` }}
-        className="background-image"
-      ></div>
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
-    </div>
-  );
-};
+import { withRouter } from "react-router-dom";
+//higner order component, takes component as an argument
+//and returns modified component
 
-export default MenuItem;
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      className="background-image"
+    ></div>
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default withRouter(MenuItem); // wrapping component gives us access to history

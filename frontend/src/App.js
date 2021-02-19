033351d4cab6d760d5a016bb51f4c12eda27db1d
Martin Blahutka <blahec@Martins-MacBook-Pro.local>
2021-02-18 21:03:31 -0800

    custom form and button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignFormsPage from "./pages/sign-forms/sign-forms.component";
+
 import Header from "./components/header/header.component";
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/sign-page" component={SignFormsPage} />
       </Switch>
     </Fragment>
   );
